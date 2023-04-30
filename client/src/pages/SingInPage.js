72c2821da1b480892f30e6ae46fddeeef6408c84
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2023-05-01 01:50:29 +0300

    Authorization and getting a token are rewritten in GraphQL.

--- client/src/pages/SingInPage.js
+++ client/src/pages/SingInPage.js
@@ -1,36 +1,53 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import {gql, useApolloClient } from '@apollo/client';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SignInForm from '../components/SignInForm';
 
-function SignInPage() {
+const LOGIN = gql`
+  query Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      userId
+      token
+    }
+  }
+`;
 
+function SignInPage() {
     const [err, setErr] = useState(null); 
-    const navigate = useNavigate();   
+    const navigate = useNavigate(); 
+    const client = useApolloClient();
   
     const signIn = async (body) => { 
-        const formData = new FormData();        
-        formData.append('email', body.email);
-        formData.append('password', body.password);
-    
-        await fetch('/api/users/login', {
-            method: 'POST',
-            credentials: 'include',
-            body: formData
-        })
-        .then((response) => {
-            
-            if (response.status === 400 || response.status === 403) {
+      client.query({
+        query: LOGIN,
+        variables: {email: body.email, password: body.password},
+      })
+      .then(result => {
+         if (result.data && result.data.login) {
+          var tokenData = result.data.login;
+          console.log(tokenData);
+
+          if (tokenData.token !== 'Login error') {
+            if (tokenData.token === 'Invalid password' || tokenData.token === 'User not found') {
                 setErr('Неверно указан email или пароль');
             } else {
+                document.cookie = tokenData.token + '; max-age=3600; path=/;';
+                document.cookie = tokenData.userId + '; max-age=3600; path=/;';
+                console.log(document.cookie);
                 setErr(null);
+  
                 navigate("/");
-            }          
-        })
-        .catch((err) => {
-            console.log(err.message);
-        });
+                window.location.reload();                    
+            }
+          } else {
+            console.log('Ошибка :\\ ');
+        }
+      }})
+      .catch(error => {
+        console.error(error);
+      });
     };
   
     return (
